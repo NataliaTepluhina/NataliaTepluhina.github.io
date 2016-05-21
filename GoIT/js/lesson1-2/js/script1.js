485f8cebcfb56899675cf5856f20b50b3c09777c
NataliaTepluhina <tarya.se@gmail.com>
2016-05-21 12:42:25 +0300

    JS Homework1-2 added

---
+++ GoIT/js/lesson1-2/js/script1.js
@@ -0,0 +1,28 @@
+var base = +prompt('Enter base number:');
+a= (isNaN(base) === true) || (base != Math.round (base));
+
+
+
+while ((isNaN(base) === true) || (base != Math.round (base)) === true)  {
+    var base = prompt('Base is not a valid integer number. Please enter correct one:');
+}
+
+var exponent = +prompt('Enter exponent:');
+while ((isNaN(exponent) === true) || (exponent != Math.round (exponent)) === true) {
+    var exponent = prompt('Exponent is not a valid integer number. Please enter correct one:');
+}
+console.log ('Result is', pow(base, exponent) );
+
+function pow (b, e) {
+    var result = 1;
+    var exp = Math.abs(e);
+    for (var i = 1; i <= exp; i++) {
+        if (e>0) {
+            result *= b;
+        }
+        else {
+            result *= 1/b;
+        }
+    }
+    return result;
+}
