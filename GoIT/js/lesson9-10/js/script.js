d3776d05b3112ec0d256b6f345eb06509b09c794
NataliaTepluhina <tarya.se@gmail.com>
2016-05-31 13:41:20 +0300

    carousel added

--- GoIT/js/lesson9-10/js/script.js
+++ GoIT/js/lesson9-10/js/script.js
@@ -2,4 +2,19 @@ $(function() {
     $('.jcarousel').jcarousel({
         
     });
+
+     $('.jcarousel-prev').jcarouselControl({
+        target: '-=1'
+    });
+
+    $('.jcarousel-next').jcarouselControl({
+        target: '+=1'
+    });
+
+    $('.jcarousel-pagination').jcarouselPagination({
+        item: function(page) {
+            return '<a href="#' + page + '">' + page + '</a>';
+        }
+    });
+
 });
