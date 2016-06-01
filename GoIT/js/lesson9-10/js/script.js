10767651338645653cc156dd63f1c5f6918e728f
NataliaTepluhina <tarya.se@gmail.com>
2016-06-01 11:38:11 +0300

    Test

--- GoIT/js/lesson9-10/js/script.js
+++ GoIT/js/lesson9-10/js/script.js
@@ -12,13 +12,13 @@ $(function() {
         target: '+=1'
     });
 
-    $('.jcarousel-pagination')
-    .on('jcarouselpagination:active', 'a', function() {
-        $(this).addClass('active');
-        })
-    .on('jcarouselpagination:inactive', 'a', function() {
-        $(this).removeClass('active');
-    });
+    // $('.jcarousel-pagination')
+    // .on('jcarouselpagination:active', 'a', function() {
+    //     $(this).addClass('active');
+    //     })
+    // .on('jcarouselpagination:inactive', 'a', function() {
+    //     $(this).removeClass('active');
+    // });
 
     $('.jcarousel-pagination').jcarouselPagination({
         item: function(page) {
