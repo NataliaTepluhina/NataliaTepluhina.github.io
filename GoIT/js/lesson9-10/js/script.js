10a102267aff8b42fb95294db13f2508cf25c89f
NataliaTepluhina <tarya.se@gmail.com>
2016-06-02 14:36:21 +0300

    Homework 9-10 added

--- GoIT/js/lesson9-10/js/script.js
+++ GoIT/js/lesson9-10/js/script.js
@@ -1,6 +1,32 @@
 $(window).load(function(){
 
     $(function() {
+        $('.dropdown').hover(
+            function(){
+                $(this).children('.submenu').slideDown(200).animate({backgroundColor:'#d8bfd8'},200);
+            },
+            function(){
+                $(this).children('.submenu').animate({backgroundColor:'#d4d4d4'},200).slideUp(200);
+            }
+        );
+
+        $('.dropdown_2').hover(
+            function(){
+                $(this).children('.submenu').animate({width:'toggle'},200).animate({backgroundColor:'#c2abc2'},200);
+            },
+
+            function(){
+                $(this).children('.submenu').animate({backgroundColor:'#d4d4d4'},200).animate({width:'toggle'},200);
+            }
+
+        );
+
+        $('.test').click(
+            function(){
+                $(this).animate({backgroundColor:'#d3a0f0'},200);
+            }    
+        );
+
         $('.jcarousel').jcarousel({
             animation: 'slow',
             wrap: 'circular'
