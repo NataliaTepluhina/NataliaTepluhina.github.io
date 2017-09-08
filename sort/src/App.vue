87a461d41cbf763098921310c559b1716bba00db
NataliaTepluhina <tarya.se@gmail.com>
2017-09-08 18:37:24 +0300

    - added sorting and searching

--- sort/src/App.vue
+++ sort/src/App.vue
@@ -1,33 +1,31 @@
 <template>
     <div id="app">
         <div class="container">
-            <button class="btn btn-primary" @click="getData">Get Data</button>
 
-            <pagination for="orders" :records="orders.length" :per-page="10"></pagination>
+            <input type="text" v-model="searchParameter" placeholder="Search in list">
 
-            <table v-if="orders.length" class="table">
+            <pagination ref="pagination" for="filteredOrders" :records="filteredOrders.length" :per-page="10"></pagination>
+
+            <table v-if="orders.length" class="table table-striped table-bordered">
                 <tbody>
                 <tr>
-                    <th>#</th>
-                    <th>Order Id</th>
-                    <th>Ship Country</th>
-                    <th>Ship City</th>
-                    <th>Currency</th>
-                    <th>Ship Date</th>
-                    <th>Latitude</th>
-                    <th>Status</th>
-                    <th>Type</th>
+                    <th v-for="column in columns"
+                        @click="sort(column.id)"
+                        :class="{'active' : sortParameter === column.id, 'reverse': sortParameter === column.id && reverse}">
+                        {{ column.text }}
+                    </th>
+
                 </tr>
                 <tr v-for="order in currentOrders">
                     <td>{{ order.RecordID }}</td>
-                    <td>{{ order.OrderID }}</td>
-                    <td>{{ order.ShipCountry }} - {{ order.ShipCity }}</td>
-                    <td>{{ order.ShipCity }}</td>
-                    <td>{{ order.Currency }}</td>
-                    <td>{{ order.ShipDate }}</td>
-                    <td>{{ order.Latitude }}</td>
-                    <td>{{ statuses[order.Status - 1] }}</td>
-                    <td>{{ types[order.Type - 1] }}</td>
+                    <td class="col-md-2">{{ order.OrderID }}</td>
+                    <td class="col-md-2">{{ order.ShipCountry }} - {{ order.ShipCity }}</td>
+                    <td class="col-md-2">{{ order.ShipCity }}</td>
+                    <td class="col-md-1">{{ order.Currency }}</td>
+                    <td class="col-md-1">{{ order.ShipDate }}</td>
+                    <td class="col-md-1">{{ order.Latitude }}</td>
+                    <td class="col-md-1">{{ statuses[order.Status - 1] }}</td>
+                    <td class="col-md-1">{{ types[order.Type - 1] }}</td>
                 </tr>
                 </tbody>
             </table>
@@ -42,7 +40,50 @@
         data() {
             return {
                 orders: [],
-                currentOrders: [],
+                columns: [
+                    {
+                        id: 'RecordID',
+                        text: '#'
+                    },
+                    {
+                        id: 'OrderID',
+                        text: 'Order ID'
+                    },
+                    {
+                        id: 'ShipCountry',
+                        text: 'Ship Country'
+                    },
+                    {
+                        id: 'ShipCity',
+                        text: 'Ship City'
+                    },
+                    {
+                        id: 'Currency',
+                        text: 'Currency'
+                    },
+                    {
+                        id: 'ShipDate',
+                        text: 'ShipDate'
+                    },
+                    {
+                        id: 'Latitude',
+                        text: 'Latitude'
+                    },
+                    {
+                        id: 'Status',
+                        text: 'Status'
+                    },
+                    {
+                        id: 'Type',
+                        text: 'Type'
+                    },
+
+                ],
+                reverse: false,
+                searchParameter: '',
+                page: 1,
+                keys: ['RecordID', 'OrderID', 'ShipCountry', 'ShipCity', 'Currency', 'ShipDate', 'Latitude'],
+                sortParameter: '',
                 statuses: ['Pending', 'Delivered', 'Canceled', 'Success', 'Info', 'Danger'],
                 types: ['Online','Retail', 'Direct'],
 
@@ -54,15 +95,35 @@
         },
 
         methods: {
-            getData() {
-                this.resource.fetchData()
-                    .then(response => response.json())
-                    .then(data => {
-                        this.orders = data.data;
-                        for (let i = 0; i < 10; i++) {
-                            this.currentOrders.push(this.orders[ i ])
-                        }
-                    })
+            sort (parameter) {
+                this.reverse = this.sortParameter === parameter ? !this.reverse : false;
+                this.sortParameter = parameter;
+            }
+
+        },
+
+        computed: {
+            filteredOrders() {
+                let reverse = this.reverse ? 'desc' : 'asc';
+                return _.orderBy(this.orders, this.sortParameter, reverse).filter(order => {
+                    if (this.searchParameter) {
+                        this.$refs['pagination'].setPage(1);
+                        return Object.keys(order).some(key => {
+                            if(order[key] && this.keys.indexOf(key) > -1) {
+                                return order[key].toString().toLowerCase().includes(this.searchParameter.trim().toLowerCase());
+                            }
+                        });
+                    }
+                    else {
+                        return true;
+                    }
+                });
+            },
+
+            currentOrders() {
+                return this.filteredOrders.filter((element, index) => {
+                    return (index  >= (this.page - 1) * 10) && (index  < this.page * 10);
+                });
             }
         },
 
@@ -71,11 +132,13 @@
                 fetchData: { method: 'GET'}
             };
             this.resource = this.$resource('default.php', {}, customActions);
-            PaginationEvent.$on('vue-pagination::orders', page => {
-                this.currentOrders = [];
-                for (let i = 0; i < 10; i++) {
-                    this.currentOrders.push(this.orders[ i + (page - 1) * 10 ])
-                }
+            this.resource.fetchData()
+                .then(response => response.json())
+                .then(data => {
+                    this.orders = data.data;
+                });
+            PaginationEvent.$on('vue-pagination::filteredOrders', page => {
+                this.page = page;
             });
         }
     }
@@ -83,5 +146,25 @@
 </script>
 
 <style lang="scss">
+    th {
+        cursor: pointer;
+    }
 
+    .active {
+        color: #337ab7;
+        position: relative;
+        
+        &:after {
+            content: '\2193';
+            position: absolute;
+            padding-left: 5px;
+            top: 7px;
+        }
+
+        &.reverse {
+            &:after {
+                content: '\2191';
+            }
+        }
+    }
 </style>
--- sort/src/main.js
+++ sort/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import VuePagination from 'vue-pagination-2';
+import lodash from 'lodash'
+import VueLodash from 'vue-lodash/dist/vue-lodash.min'
 import App from './App.vue'
 
 Vue.use(VueResource);
 Vue.use(VuePagination);
+Vue.use(VueLodash, lodash);
 
 Vue.http.options.root = 'http://keenthemes.com/metronic/preview/inc/api/datatables/demos/';
 
